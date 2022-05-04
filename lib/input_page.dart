dd3b7867dffc823ad5a6b76371527c20e707359d
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-04 17:32:29 +0900

    形作り済み

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,13 +17,60 @@ class _InputPageState extends State<InputPage> {
             "BMI 計算",
           ),
         ),
-        body: Container(
-          margin: EdgeInsets.all(15.0),
-          decoration: BoxDecoration(
-              color: Color(0xFF1DE33),
-              borderRadius: BorderRadius.circular(10.0)),
-          height: 200.0,
-          width: 170.0,
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1DE33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1DE33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1DE33),
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                        color: Color(0xFF1DE33),
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                        color: Color(0xFF1DE33),
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ],
+            ))
+          ],
         ));
   }
 }
