b13386d1ec1b6750f38f042ec73acaf54dd66393
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-04 16:47:32 +0900

    不要なコード削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
+
 void main() {
   runApp(const BMICalculator());
 }
@@ -21,34 +23,6 @@ class BMICalculator extends StatelessWidget {
         ),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
-
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("BMI CALCULATOR"),
-      ),
-      body: Center(
-        child: Text("Body Text"),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(accentColor: Colors.green),
-        child: FloatingActionButton(
-          onPressed: () {  },
-          child: Icon(Icons.add),
-        ),
-      ),
     );
   }
 }
