b5c447a92344eaa5f4664f48a55266852a46d552
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-06-09 21:23:53 +0900

    icon_content改修

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   IconContent({required this.icon, required this.label});
@@ -20,6 +21,7 @@ class IconContent extends StatelessWidget {
         ),
         Text(
           label,
+          style: labelTextStyle,
         )
       ],
     );
