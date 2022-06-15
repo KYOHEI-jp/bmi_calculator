191e6c6e1965742014a8998e92ace18959d0f6be
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-06-15 21:00:56 +0900

    results_pageのコンストラクター更新

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -4,7 +4,16 @@ import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
-  const ResultsPage({Key? key}) : super(key: key);
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  const ResultsPage(
+      {Key? key,
+      required this.bmiResult,
+      required this.resultText,
+      required this.interpretation})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
